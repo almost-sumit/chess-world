e962fe59c3591b64fe634200864b81d63d20b964
Sumit Tamgale <sumittamgale@gmail.com>
2019-04-03 16:24:55 -0500

    initialed project repo

---
+++ src/application/User.java
@@ -0,0 +1,25 @@
+package application;
+
+public class User {
+	
+	private String userId;
+	
+	private String userName;
+	
+	private String password;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String emailId;
+	
+	public String createAccount() {
+		return this.userId;
+	}
+	
+	public boolean deleteAccount() {
+		return true;
+	}
+
+}
